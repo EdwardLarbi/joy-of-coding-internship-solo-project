287ddfceef6b7d333cfc12e212e2ee115238dd22
Edward Larbi <edwardlarbi62@icloud.com>
2024-03-01 09:58:43 -0500

    create nav bar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,35 @@
+'use client'; 
+
+import Link from 'next/link';
+import React from 'react'
+import { GrTasks } from "react-icons/gr";
+
+
+const NavBar = () => {
+    const navLinks = [
+        {href: '/', label: 'Dashboard'},
+        {href: '/organize', label: 'Organize'}
+    ]
+
+
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <GrTasks />
+        <ul className='flex space-x-6'>
+            {
+                navLinks.map((link) =>
+                    <li key={link.href}>
+                        <Link href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+                            {link.label}
+                        </Link>
+                    </li>
+                )
+            }
+        </ul>
+        
+    </nav>
+  )
+}
+
+export default NavBar
