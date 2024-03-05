215b35db5e3a37862c839d87aa1e45de44451953
Edward Larbi <edwardlarbi62@icloud.com>
2024-03-05 12:29:35 -0500

    display tasks on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-import { Button } from '@radix-ui/themes';
+import { Button, Table } from '@radix-ui/themes';
+import prisma from "@/prisma/client"
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const myTasks = await prisma.task.findMany();
+  console.log(myTasks);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button><Link href='/new'>Create Task</Link></Button>
+
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Task</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Due Date</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {
+            myTasks.map(
+              (myTask)=>
+              <Table.Row>
+                <Table.RowHeaderCell>{myTask.name}</Table.RowHeaderCell>
+                <Table.Cell>{myTask.description}</Table.Cell>
+                <Table.Cell>{myTask.dueDate}</Table.Cell>
+              </Table.Row>
+            )
+          }
+        </Table.Body>
+      </Table.Root>
     </main>
   );
 }
